5278d29cf7450e2a57136adc7468ed2cffdefa5d
LazzariPaolo <lazzari.paolok@gmail.com>
2018-04-08 09:00:11 +0000

    Plotpy image working on rasperriPi

--- parsing.py
+++ parsing.py
@@ -1,8 +1,11 @@
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import sys
+import plotly
+from plotly.graph_objs import Scatter, Layout
+import plotly.offline as offline
 import numpy as np
 from datetime import datetime
-from time import gmtime, strftime, sleep
+from time import gmtime, strftime
 from math import sqrt
 f = open('HolyData.txt','r')
 lines = f.read().split("\n")
@@ -10,27 +13,18 @@ AI = []
 QI = []
 amplitude = []
 base = 10
-#i = lines.split(' ',1)[0]
-#q = lines.split(' ',1)[1]
-while True:
-    #i_star = [i.split(' ')[0] for i in lines]
-    for i in lines[1:]:
-        split_string = i.split();
-        #print(split_string)
-        AI.append(split_string[0])
-        QI.append(split_string[1])
-        #print(type(int(split_string[0])))
-        amplitude.append(sqrt(pow(2,int(split_string[0])+pow(2,int(split_string[1])))));
-    
-    f.close()
-    amplitude = np.asarray(amplitude)
-    samples = np.arange(0,len(lines)-1,1)
-    fig, ax = plt.subplots()
-    plt.plot(samples,amplitude)
-    plt.title('Wifi')
-    plt.ylabel('Magnitude')
-    plt.xlabel('Samples',fontdict=None, labelpad=None)
-    fig.savefig('Screenshots/Magnitude.png')
-    plt.show()
-    time.sleep(1)
-sys.exit()
+
+for i in lines[1:]:
+    split_string = i.split();
+    AI.append(split_string[0])
+    QI.append(split_string[1])
+    amplitude.append(sqrt(pow(2,int(split_string[0])+pow(2,int(split_string[1])))));
+f.close()
+
+samples = list(range(0,len(lines)-1,1))
+
+plotly.offline.plot(
+    {
+    "data": [Scatter(x=samples,y=amplitude)],
+    "layout": Layout(title="hello world")},
+    image='png',auto_open=False)
